ee5adf327134e026ae2b3e809446f1ebad46092d
vipulroxx <vipulsharma936@gmail.com>
2020-02-09 07:57:11 +0530

    Added button event

--- crop-prediction/src/app/app.component.ts
+++ crop-prediction/src/app/app.component.ts
@@ -1,16 +1,5 @@
-import { Component, OnInit, ViewChild, Injectable, Inject } from '@angular/core';
+import { Component, OnInit, ViewChild, Injectable } from '@angular/core';
 import {MatPaginator, MatTableDataSource} from '@angular/material';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json' ,
-    'Access-Control-Allow-Origin': 'true'
-  })
-};
 export interface UserData {
   Label: string;
   EventId: number;
@@ -18,57 +7,19 @@ export interface UserData {
   DER_mass_transverse_met_lep: number;
   DER_mass_vis: number;
 }
-export interface DialogData {
-  Rainfall: number;
-}
-interface SearchResults {
-  prediction: number;
-}
-
-
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
+export class AppComponent implements OnInit{
+  title = 'AgroB';
 
-export class AppComponent implements OnInit {
-
-title: 'AgroB';
-totalAngularPackages;
-Rainfall: number;
-
-
-    constructor(public dialog: MatDialog) {}
-
-    openDialog(): void {
-      // this.http.get<SearchResults>('http://aae72774.ngrok.io/predict', httpOptions).subscribe(data => {
-      // this.Rainfall = data.prediction;
-      const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-        width : '250px',
-        data : {Rainfall: 5112162}
-    });
-// });
-}
-
-
+  handleClick(event: Event) {
+    console.log('Click!', event)
+  }
 
   ngOnInit() {
+
  }
 }
-
-@Component({
-  templateUrl: 'dialog-overview-example-dialog.html'
-})
-
-export class DialogOverviewExampleDialog {
-  constructor(
-    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
-    onNoClick(): void {
-      this.dialogRef.close();
-    }
-}
-
-
-
