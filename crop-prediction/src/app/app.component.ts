5613571d64d50dda1f8d9810e55f49841e8a27ab
vipulroxx <vipulsharma936@gmail.com>
2020-02-09 08:14:12 +0530

    Modified button

--- crop-prediction/src/app/app.component.ts
+++ crop-prediction/src/app/app.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit, ViewChild, Injectable } from '@angular/core';
-import {MatPaginator, MatTableDataSource} from '@angular/material';
-export interface UserData {
-  Label: string;
-  EventId: number;
-  DER_mass_MMC: number;
-  DER_mass_transverse_met_lep: number;
-  DER_mass_vis: number;
-}
+import {MatPaginator, MatTableDataSource, MatDialogRef, MatDialog} from '@angular/material';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'AgroB';
+export class AppComponent{
+  selectedOption: string;
 
-  handleClick(event: Event) {
-    console.log('Click!', event)
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    let dialogRef = this.dialog.open(DialogResultExampleDialog);
+    dialogRef.afterClosed().subscribe(result => {
+      this.selectedOption = result;
+    });
   }
+}
 
-  ngOnInit() {
-
- }
+@Component({
+  selector: 'dialog-result-example-dialog',
+  templateUrl: './dialog-result-example-dialog.html',
+})
+export class DialogResultExampleDialog {
+  constructor(public dialogRef: MatDialogRef<DialogResultExampleDialog>) {}
 }
